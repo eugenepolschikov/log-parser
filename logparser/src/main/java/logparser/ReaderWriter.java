4b3ed1666d864321a93cdeed1af38ea32c703f95
eugene polschikov <eugene.polschikov@gmail.com>
2020-02-11 18:20:46 +0300

    Updated logic, separated into constants.

--- logparser/src/main/java/logparser/ReaderWriter.java
+++ logparser/src/main/java/logparser/ReaderWriter.java
@@ -11,19 +11,21 @@ import java.util.Scanner;
 
 public class ReaderWriter {
 
-    protected static final Logger log = LoggerFactory.getLogger(ReaderWriter.class);
+    private static final Logger log = LoggerFactory.getLogger(ReaderWriter.class);
+    private static final String LOG_FULL_FILEPATH_TXT_FILE = "D:\\exadel_projects\\2019-09(verint)\\java_repo\\log-parser\\logparser\\2223334445555.txt";
+    private static final String MANAGER_USERNAME_OCCURENCY = "Login.c(158): username";
+    private static final String TOTAL_RESOURCE_COUNT_FIELD = "totalResourceCount";
 
 
     public static void main(String[] args) throws FileNotFoundException {
-        String fileName = "D:\\exadel_projects\\2019-09(verint)\\java_repo\\logparser\\2223334445555.txt"; //this path is on my local
-        InputStream inputStream = new FileInputStream(fileName);
+        InputStream inputStream = new FileInputStream(LOG_FULL_FILEPATH_TXT_FILE);
 
         try (Scanner fileScanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
             while (fileScanner.hasNextLine()) {
                 String line = fileScanner.nextLine();
-                if (line.contains("Login.c(158): username")) {
+                if (line.contains(MANAGER_USERNAME_OCCURENCY)) {
                     log.info(line);
-                } else if (line.contains("totalResourceCount")) {
+                } else if (line.contains(TOTAL_RESOURCE_COUNT_FIELD)) {
                     log.info(line);
                     log.info("##################");
                 }
